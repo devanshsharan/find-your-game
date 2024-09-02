2003ce1519a9aca30fb84dd5c37d1ab2f114da20
devansh1356 <devanshsharan.gupta@beehyv.com>
2024-09-02 20:04:30 +0530

    no image case finally handled

--- game-hub/src/components/image-url.ts
+++ game-hub/src/components/image-url.ts
@@ -1,8 +1,8 @@
 import nImage from '../assets/no-image-placeholder-6f3882e0.webp'
 
 const getCroppedImageUrl = (url: string) => {
-    if(!url) return '';
-    const target = nImage;
+    if(!url) return nImage;
+    const target = 'media/';
    const index = url.indexOf(target) +target.length;
    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
